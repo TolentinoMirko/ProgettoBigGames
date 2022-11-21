9e692e6593edac88b75b2862583a263433c2df87
TolentinoMirko <tolentino.mirko@iisgalvanimi.edu.it>
2022-11-21 10:45:29 +0000

    backend in corso

--- biggame.py
+++ biggame.py
@@ -10,7 +10,7 @@ def backend():
   citta_negozio = request.args['citta_shop']
   via_negozio = request.args['via_shop']
   conn = pymssql.connect(server = '213.140.22.237\SQLEXPRESS',user = 'tolentino.mirko',password = 'xxx123##',database = 'tolentino.mirko')
-  query = f"Select * from Shops inner join Location on Shops.id_shops = Location.id_shops inner join Games on Location.id_games = Games.id_games WHERE Shops.indirizzo_shop = '{via_negozio}' and Shops.citta_negozio = '{citta_negozio}'"
+  query = f"Select * from Shops inner join Location on Shops.id_shop = Location.id_shops inner join Games on Location.id_games = Games.id_games WHERE Shops.indirizzo_shop = '{via_negozio}' and Shops.citta_negozio = '{citta_negozio}'"
   tab_giochi_quantita = f"Select Shop."
   df = pd.read_sql(query,conn)
   
